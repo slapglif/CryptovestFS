d39158f2cd7b39db47194811d591d4a31fca00bb
kuro112 <kuro@xtcr.net>
2015-07-25 11:37:45 -0700

    inital commit

--- app/static/js/zerif.js
+++ app/static/js/zerif.js
@@ -4,9 +4,7 @@
 $.vegas('slideshow', {
   delay:7000,
   backgrounds:[
-    { src:'images/backgrounds/bg1.jpg', fade:1000 },
-    { src:'images/backgrounds/bg2.jpg', fade:1000 },
-    { src:'images/backgrounds/bg3.jpg', fade:1000 }
+    { src:'http://puu.sh/jcyr7/d314a815a0.jpg' }
   ]
 });
       
